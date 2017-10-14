3f3cec40177fb0922497b6aa622c90c4e205c5b4
Nanthawad <5806021622123@fitm.kmutnb.ac.th>
2017-10-15 00:07:41 +0700

    Nanthawad and Nutdanai

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<template>
+<section class="container">
+  <div>
+  <h1 id="head">ADIDAS SHOPS</h1>
+  </div>
+  <div id="body">
+  <table style="width:100%">
+  <center>
+  <tr>
+    <th>Iniki Runner</th>
+    <th>EQT Support 93/17</th>
+    <th>Tubular Shadow Shoes</th>
+    <th>Cloudfoam Racer TR</th>
+  </tr>
+  <tr>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BY9732_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 5,290 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BZ0586_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 7,990 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BY9740_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 4,990 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BC0055_FM_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 2,690 บาท</td>
+  </tr>
+  </center>
+  </table>
+  </div><br><br>
+  <div id="body">
+  <table style="width:100%">
+  <center>
+  <tr>
+    <th>Superstar Boost Shoes</th>
+    <th>UltraBOOST ST</th>
+    <th>Tubular Doom Sock Shoes</th>
+    <th>Cloudfoam Racer TR</th>
+  </tr>
+  <tr>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BZ0206_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 4,690 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/S80616_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 7,290 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BY3563_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 4,990 บาท</td>
+    <td><img src = "http://d12vfgykacfet6.cloudfront.net/tmp/catalog/product/large/BC0119_SL_eCom.jpg" width="250px" height="250px"><br><button class="button button2">ซื้อ</button> ราคา : 2,690 บาท</td>
+  </tr>
+  </center>
+  </table>
+  </div>
+  </section>
+
+  </template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+#body{
+  text-align: center;
+}
+#head{
+   text-align: center;
+   font-size: 60px;
+   margin-top: 10px;
+   color: #E77A26;
+}
+
+.button {
+    background-color: #E77A26; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 40px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+.button2 {
+    background-color: white;
+    color: black;
+    border: 2px solid #008CBA;
+}
+
+</style>
